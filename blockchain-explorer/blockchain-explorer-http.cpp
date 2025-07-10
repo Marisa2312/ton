df4825dfb356ae0e09cc6c4d3429c212eea0d3f9
Marisa2312 <182423094+Marisa2312@users.noreply.github.com>
2025-07-10 06:46:10 +0200

    marisa2312

--- blockchain-explorer/blockchain-explorer-http.cpp
+++ blockchain-explorer/blockchain-explorer-http.cpp
@@ -1,5 +1,4 @@
-/*
-    This file is part of TON Blockchain source code.
+/* This file is part of TON Blockchain source code.
 
     TON Blockchain is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License
